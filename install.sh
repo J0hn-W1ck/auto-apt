6409062e9bf8aaece6935d48888197d4191b35a3
John-W1ck <johnwickhacker@hotmail.com>
2016-12-26 18:34:16 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ chmod 777 auto-apt.sh
 mv auto-apt.sh auto-apt
 mv auto-apt /usr/local/bin
 echo
-echo -e "\e[1;32m AUTO-APT INSTALADO CORRECTAMENTE  Ejecute desde la terminal el comando \e[0m\e[1;31msudo auto-apt\e[0m"
+echo -e "\e[1;32m AUTO-APT INSTALADO CORRECTAMENTE..\e[0m"
+echo -e "\e[1;32m Ejecute desde la terminal el comando \e[0m\e[1;31msudo auto-apt\e[0m"
 echo
 else
 echo
